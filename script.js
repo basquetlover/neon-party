30828e8584f68a76812e58390986bd757bf50bbd
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-05-06 13:16:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// const tituloNeon = document.querySelector('.titulo');
+// tituloNeon.style.setProperty('--rand', `${Math.random()}s`);
+// const tituloNeonGreen = document.querySelector('.titulo section');
+// tituloNeonGreen.style.setProperty('--rand2', `${Math.random()}s`);
